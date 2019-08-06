d3c4e0bb7f560cf7007c7946165d228fde0de478
Brian <brian.keefe@optum.com>
2019-08-06 15:06:42 -0400

    post method
    Signed-off-by: Brian <brian.keefe@optum.com>

--- server.js
+++ server.js
@@ -4,25 +4,54 @@ const app = express();
 const port = 3001;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-let collection;
-const MongoClient = require("mongodb").MongoClient;
-const uri =
-	"mongodb+srv://new-user:123@cluster0-gzyjc.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-	if (err) {
-		return console.log(err);
-	}
-	const collection = client.db("test").collection("devices");
-	// perform actions on the collection object
-	client.close();
+
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+const schema = new Schema({
+	word: String,
+	pos: String,
+	def: String,
+});
+const Card = mongoose.model("Card", schema);
+const word = new Card({ word: "test", pos: "test", def: "test" });
+word.save();
+Card.find(function(err, words) {
+	if (err) return console.error(err);
+	console.log(words);
+});
+Card.remove({}, function(err) {
+	console.log("collection removed");
+});
+mongoose.connect("mongodb://localhost/flashcards", {
+	useNewUrlParser: true,
 });
 
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+	console.log("CONNECTED");
+});
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.get("/hello", (req, res) => res.send("whats good"));
 
-app.post("/", (req, res) => {
-	console.log(req.body);
-	res.send(req.body);
+app.post("/word", (req, res) => {
+	let word = new Card({
+		word: req.body.word,
+		pos: req.body.pos,
+		def: req.body.def,
+	});
+	word.save();
+	Card.find(function(err, words) {
+		if (err) return console.error(err);
+		let x = words;
+		console.log(x);
+		res.send(JSON.stringify(x));
+	});
+});
+
+app.listen(port, () => {
+	console.log("server is now listening on 3001");
 });
