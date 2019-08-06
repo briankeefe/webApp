139d467a1df0d905791704005a38207382296dbd
Brian <brian.keefe@optum.com>
2019-08-06 13:01:52 -0400

    mongo
    Signed-off-by: Brian <brian.keefe@optum.com>

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3001;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+let collection;
+const MongoClient = require("mongodb").MongoClient;
+const uri =
+	"mongodb+srv://new-user:123@cluster0-gzyjc.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+	if (err) {
+		return console.log(err);
+	}
+	const collection = client.db("test").collection("devices");
+	// perform actions on the collection object
+	client.close();
+});
+
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get("/hello", (req, res) => res.send("whats good"));
+
+app.post("/", (req, res) => {
+	console.log(req.body);
+	res.send(req.body);
+});
