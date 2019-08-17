abfb164fecc2e413aa7949c4118b1e6d67974c15
Brian Keefe <briankeefe@umass.edu>
2019-08-17 10:37:09 -0400

    Reverted to old router

--- src/depricated/index.js
+++ src/depricated/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import Bar from "./depricated/stuff.jsx/index.js";
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from "../serviceWorker";
+import IndexPage from "../../pages/Index";
 
-ReactDOM.render(<Bar />, document.getElementById("root"));
+ReactDOM.render(<Thing />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
