ff68af0256aca343c4e971305291ade3975f3523
briankeefe <briankeefe@umass.edu>
2019-08-20 09:19:18 -0400

    Successfully Authenticated

--- pages/auth.js
+++ pages/auth.js
@@ -2,6 +2,7 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import * as firebase from "firebase";
 import { createMuiTheme } from "@material-ui/core";
 import { withStyles } from "@material-ui/styles";
+import react, { useState, useEffect } from "react";
 
 const theme = createMuiTheme({
 	spacing: factor => [0, 4, 8, 16, 32, 64][factor],
@@ -16,21 +17,27 @@ const styles = theme => ({
 
 function AuthPage(props) {
 	const { classes } = props;
+
+	if (!firebase.apps.length) {
+		let firebaseConfig = {
+			apiKey: "AIzaSyAwMIqSwJE9iL0ZlWyd3hrepk5Mymn7lSI",
+			authDomain: "flashcards-cda38.firebaseapp.com",
+			databaseURL: "https://flashcards-cda38.firebaseio.com",
+			projectId: "flashcards-cda38",
+			storageBucket: "flashcards-cda38.appspot.com",
+			messagingSenderId: "872718028889",
+			appId: "1:872718028889:web:a95585059bf6d551",
+		};
+		// Initialize Firebase
+		firebase.initializeApp(firebaseConfig);
+	}
+
 	const [user, loading, error] = useAuthState(firebase.auth());
 
-	var firebaseConfig = {
-		apiKey: "AIzaSyAwMIqSwJE9iL0ZlWyd3hrepk5Mymn7lSI",
-		authDomain: "flashcards-cda38.firebaseapp.com",
-		databaseURL: "https://flashcards-cda38.firebaseio.com",
-		projectId: "flashcards-cda38",
-		storageBucket: "flashcards-cda38.appspot.com",
-		messagingSenderId: "872718028889",
-		appId: "1:872718028889:web:a95585059bf6d551",
-	};
-	// Initialize Firebase
-	firebase.initializeApp(firebaseConfig);
 	const login = () => {
-		firebase.auth().signInWithEmailAndPassword("test@test.com", "password");
+		firebase
+			.auth()
+			.signInWithEmailAndPassword("briguy100@gmail.com", "111397");
 	};
 
 	const logout = () => {
